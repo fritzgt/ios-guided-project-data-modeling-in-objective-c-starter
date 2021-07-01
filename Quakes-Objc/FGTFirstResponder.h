8546cb3a833ce07afe856efa01e9803506ff09e1
exoticproductions <contactexoticproductions@gamail.com>
2021-07-01 15:07:49 +0300

    aaded first responder class

---
+++ Quakes-Objc/FGTFirstResponder.h
@@ -0,0 +1,20 @@
+//
+//  FGTFirstResponder.h
+//  Quakes-Objc
+//
+//  Created by FGT MAC on 7/1/21.
+//  Copyright © 2021 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FGTFirstResponder : NSObject
+
+@property (nonatomic, copy) NSString *name;
+ 
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Quakes-Objc/FGTFirstResponder.m
@@ -0,0 +1,13 @@
+//
+//  FGTFirstResponder.m
+//  Quakes-Objc
+//
+//  Created by FGT MAC on 7/1/21.
+//  Copyright © 2021 Lambda, Inc. All rights reserved.
+//
+
+#import "FGTFirstResponder.h"
+
+@implementation FGTFirstResponder
+
+@end
