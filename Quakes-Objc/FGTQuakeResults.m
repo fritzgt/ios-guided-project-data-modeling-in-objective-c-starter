940c17d3917e14c907368f7180d61bcf9242f5bc
exoticproductions <contactexoticproductions@gamail.com>
2021-07-03 10:02:02 +0300

    implemented quake array

--- Quakes-Objc/FGTQuakeResults.m
+++ Quakes-Objc/FGTQuakeResults.m
@@ -10,4 +10,48 @@
 
 @implementation FGTQuakeResults
 
+- (instancetype)init
+{
+    return [self initWithQuakes:@[]];
+}
+
+- (instancetype)initWithQuakes:(NSArray<FGTQuake *> *)quakes
+{
+    self = [super init];
+    
+    if(self){
+        _quakes = quakes;
+    }
+    
+    return  self;
+}
+
+
+- (nullable instancetype)initWithDictionary:(NSDictionary *)aDictionary
+{
+    NSArray *quakeDictionaries = [aDictionary objectForKey:@""];
+    if(![quakeDictionaries isKindOfClass:NSArray.class]) return nil;
+    
+    NSMutableArray *quakes = [[NSMutableArray alloc] initWithCapacity: quakeDictionaries.count];
+    
+    for (NSDictionary *quakeDictionary in quakeDictionaries){
+        //IF is not valid continue to the next
+        if(![quakeDictionary isKindOfClass:NSDictionary.class])continue;
+        
+        //Init to add to the array
+        FGTQuake *quake = [[FGTQuake alloc] initWithDictionary:quakeDictionary];
+        
+        if(quake){
+            [quakes addObject:quake];
+        }else{
+            NSLog(@"Unable tp parse quake dic: %@", quakeDictionary);
+        }
+        
+    }
+    
+    
+    
+    return [self initWithQuakes:@[]];
+}
+
 @end
